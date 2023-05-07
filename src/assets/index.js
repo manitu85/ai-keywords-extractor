49b0a47bcc08a0783a8f3dbf03d94ddb2bd21613
Alex Burke <alexburke.dev@gmail.com>
2023-05-07 21:44:11 +0300

    style: add assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,9 @@
+export { default as Bulb } from '@/assets/bulb.svg';
+export { default as BulbIdea } from '@/assets/bulb-idea.svg';
+export { default as BulbLighting } from '@/assets/bulb-lighting.svg';
 export { default as ChakraLogo } from '@/assets/chakraui.svg';
 export { default as EslintLogo } from '@/assets/eslint.svg';
+export { default as OpenAiLogo } from '@/assets/openai.svg';
 export { default as PrettierLogo } from '@/assets/prettier.svg';
 export { default as ReactLogo } from '@/assets/react.svg';
 export { default as ViteLogo } from '@/assets/vite.svg';
